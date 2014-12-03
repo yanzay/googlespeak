855b426de2d9a4e568aa4aa0350f4b6cb15eab49
Alexey Grachov <grachov.alexey@gmail.com>
2014-12-03 17:22:18 +0200

    Add support for long texts.
    Long texts will split into sentences, long sentences split into tokens
    (between comas).

--- googlespeak.go
+++ googlespeak.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/exec"
 	"os/user"
+	"strings"
 	"unicode/utf8"
 )
 
@@ -23,23 +24,21 @@ func Say(s string, args ...string) error {
 		lang = args[0]
 	}
 
-	err := validateParams(s, lang)
-	if err != nil {
-		return err
+	if !isValidLang(lang) {
+		return errors.New("Invalid language code!")
 	}
 
 	log.Printf("Lang: %s, say: %s", lang, s)
-	err = speak(s, lang)
-	return err
-}
-
-func validateParams(s, lang string) error {
-	if utf8.RuneCountInString(s) > 100 {
-		return errors.New("Text exceeds max char limit (100)!")
+	sentenses, err := splitSentenses(s)
+	if err != nil {
+		log.Print(err)
+		return err
 	}
 
-	if !isValidLang(lang) {
-		return errors.New("Invalid language code!")
+	err = speak(sentenses, lang)
+	if err != nil {
+		log.Print(err)
+		return err
 	}
 	return nil
 }
@@ -52,6 +51,26 @@ func isValidLang(s string) bool {
 	}
 	log.Printf("Invalid language: %s", s)
 	return false
+}
+
+func splitSentenses(s string) ([]string, error) {
+	sentenses := strings.Split(s, ".")
+	var result []string
+	for _, sentense := range sentenses {
+		if utf8.RuneCountInString(sentense) > 100 {
+			tokens := strings.Split(sentense, ",")
+			for i, token := range tokens {
+				tokens[i] = strings.TrimSpace(token)
+				if utf8.RuneCountInString(tokens[i]) > 100 {
+					return nil, errors.New("Can't split text into short tokens.")
+				}
+			}
+			result = append(result, tokens...)
+		} else {
+			result = append(result, strings.TrimSpace(sentense))
+		}
+	}
+	return result, nil
 }
 
 func getAudio(s, lang string) (io.ReadCloser, error) {
@@ -97,22 +116,30 @@ func cacheAudio(stream io.Reader, s, lang string) (io.ReadCloser, error) {
 	return f, err
 }
 
-func speak(s, lang string) error {
-	log.Printf("Get from cache %s/%s", lang, s)
-	audio, err := getFromCache(s, lang)
-	if err != nil {
-		log.Printf("Cache for %s/%s not found. Trying to get audio from Google", lang, s)
-		stream, err := getAudio(s, lang)
-		if err == nil {
-			log.Printf("Caching stream for %s/%s", lang, s)
-			audio, _ = cacheAudio(stream, s, lang)
+func speak(sentences []string, lang string) error {
+	var streams []io.ReadCloser
+	for _, s := range sentences {
+		log.Printf("Get from cache %s/%s", lang, s)
+		audio, err := getFromCache(s, lang)
+		if err != nil {
+			log.Printf("Cache for %s/%s not found. Trying to get audio from Google", lang, s)
+			stream, err := getAudio(s, lang)
+			if err == nil {
+				log.Printf("Caching stream for %s/%s", lang, s)
+				audio, _ = cacheAudio(stream, s, lang)
+			}
+		}
+		defer audio.Close()
+		streams = append(streams, audio)
+	}
+
+	for _, audio := range streams {
+		err := play(audio)
+		if err != nil {
+			return err
 		}
 	}
-	defer audio.Close()
-	err = play(audio)
-	if err != nil {
-		return err
-	}
+
 	return nil
 }
 
